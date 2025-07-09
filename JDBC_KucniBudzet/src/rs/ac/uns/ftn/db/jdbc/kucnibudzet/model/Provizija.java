981b6c4aa83492ea89c8ac691e9fb049e0bfa009
Nataša Radmilović <nata.radmilovic@gmail.com>
2025-07-09 14:54:29 +0200

    small change

--- JDBC_KucniBudzet/src/rs/ac/uns/ftn/db/jdbc/kucnibudzet/model/Provizija.java
+++ JDBC_KucniBudzet/src/rs/ac/uns/ftn/db/jdbc/kucnibudzet/model/Provizija.java
@@ -8,6 +8,41 @@ public class Provizija {
 	private double iznos;
 	private LocalDateTime datum;
 	private String tip;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public double getIznos() {
+		return iznos;
+	}
+	public void setIznos(double iznos) {
+		this.iznos = iznos;
+	}
+	public LocalDateTime getDatum() {
+		return datum;
+	}
+	public void setDatum(LocalDateTime datum) {
+		this.datum = datum;
+	}
+	public String getTip() {
+		return tip;
+	}
+	public void setTip(String tip) {
+		this.tip = tip;
+	}
+	public Provizija() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Provizija(int id, double iznos, LocalDateTime datum, String tip) {
+		super();
+		this.id = id;
+		this.iznos = iznos;
+		this.datum = datum;
+		this.tip = tip;
+	}
 	
 	
 
--- JDBC_KucniBudzet/src/rs/ac/uns/ftn/db/jdbc/kucnibudzet/model/Racun.java
+++ JDBC_KucniBudzet/src/rs/ac/uns/ftn/db/jdbc/kucnibudzet/model/Racun.java
@@ -11,5 +11,82 @@ public class Racun {
 	private double stanje;
 	private LocalDateTime datumOtvaranja;
 	private LocalDateTime datumZatvaranja;
+	private int idTipaRacuna;
+	private int idValute;
+	
+	public Racun() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Racun(int id, String naziv, double stanje, LocalDateTime datumOtvaranja, LocalDateTime datumZatvaranja,
+			int idTipaRacuna, int idValute) {
+		super();
+		this.id = id;
+		this.naziv = naziv;
+		this.stanje = stanje;
+		this.datumOtvaranja = datumOtvaranja;
+		this.datumZatvaranja = datumZatvaranja;
+		this.idTipaRacuna = idTipaRacuna;
+		this.idValute = idValute;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNaziv() {
+		return naziv;
+	}
+
+	public void setNaziv(String naziv) {
+		this.naziv = naziv;
+	}
+
+	public double getStanje() {
+		return stanje;
+	}
+
+	public void setStanje(double stanje) {
+		this.stanje = stanje;
+	}
+
+	public LocalDateTime getDatumOtvaranja() {
+		return datumOtvaranja;
+	}
+
+	public void setDatumOtvaranja(LocalDateTime datumOtvaranja) {
+		this.datumOtvaranja = datumOtvaranja;
+	}
+
+	public LocalDateTime getDatumZatvaranja() {
+		return datumZatvaranja;
+	}
+
+	public void setDatumZatvaranja(LocalDateTime datumZatvaranja) {
+		this.datumZatvaranja = datumZatvaranja;
+	}
+
+	public int getIdTipaRacuna() {
+		return idTipaRacuna;
+	}
+
+	public void setIdTipaRacuna(int idTipaRacuna) {
+		this.idTipaRacuna = idTipaRacuna;
+	}
+
+	public int getIdValute() {
+		return idValute;
+	}
+
+	public void setIdValute(int idValute) {
+		this.idValute = idValute;
+	}
+	
+	
 	
 }
